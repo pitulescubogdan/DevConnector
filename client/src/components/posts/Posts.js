7290a95867a9c137ece839b69b9d654370701148
pitulescubogdan <pitulescubogdan@gmail.com>
2020-10-06 22:38:04 +0300

    Like, unlike, delete post functionalities added

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -20,7 +20,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
       </p>
       <div className='posts'>
         {posts.map((post) => (
-          <PostItem key={post.id} post={post} />
+          <PostItem key={post._id} post={post} />
         ))}
       </div>
     </Fragment>
