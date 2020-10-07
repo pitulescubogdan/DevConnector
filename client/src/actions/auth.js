a550e759e8ffcf66daebc1158a8d9ac3d9ab2995
pitulescubogdan <pitulescubogdan@gmail.com>
2020-10-07 12:45:05 +0300

    Fix bug when refreshing the post page.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -16,18 +16,18 @@ import {
 export const loadUser = () => async (dispatch) => {
   if (localStorage.token) {
     setAuthToken(localStorage.token);
-  }
-
-  try {
-    const res = await axios.get('./api/auth');
-    dispatch({
-      type: USER_LOADED,
-      payload: res.data,
-    });
-  } catch (err) {
-    dispatch({
-      type: AUTH_ERROR,
-    });
+  } else {
+    try {
+      const res = await axios.get('./api/auth');
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: AUTH_ERROR,
+      });
+    }
   }
 };
 
